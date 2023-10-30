15d73253f1b61ea67fa1579d2ed6d503c60b3942
ssb1565b <ssongsumovie@naver.com>
2023-10-30 16:27:29 +0900

    Feat: 할일목록 수정

--- 01.javascript/todoapp/assets/js/pages/update/TodoUpdate.js
+++ 01.javascript/todoapp/assets/js/pages/update/TodoUpdate.js
@@ -1,1 +1,72 @@
+// 할일 등록
+import Header from "../../layout/Header.js";
+import Footer from "../../layout/Footer.js";
+import TodoList from "../list/TodoList.js";
+
 // 할일 수정
+const TodoUpdate = async function ({ _id } = {}) {
+  // axios.defaults.baseURL = "http://localhost:33088/api/todolist";
+  try {
+    const response = await axios(`/${_id}`);
+    if (response.data.ok) {
+      const data = await response.data;
+      const { title, content, done } = data.item;
+      const page = document.createElement("div");
+      page.setAttribute("id", "page");
+      const contentBox = document.createElement("ul");
+      const list = document.createElement("li");
+      const attributes = [
+        { label: "제목", value: title },
+        { label: "내용", value: content },
+      ];
+
+      attributes.forEach((attribute, idx) => {
+        const listItem = document.createElement("li");
+        const label = document.createElement("label");
+        const input = document.createElement("input");
+        input.id = idx;
+        label.textContent = attribute.label;
+
+        input.value = attribute.value;
+
+        listItem.appendChild(label);
+        listItem.appendChild(input);
+        list.appendChild(listItem);
+      });
+
+      const editEl = document.createElement("button");
+      editEl.innerText = "수정 완료";
+      page.appendChild(Header(`${title} 수정`));
+      page.appendChild(contentBox);
+      contentBox.appendChild(list);
+      page.appendChild(editEl);
+      page.appendChild(Footer());
+
+      editEl.addEventListener("click", async function () {
+        const titleValue = document.getElementById("0").value;
+        const contentValue = document.getElementById("1").value;
+
+        try {
+          const body = {
+            title: titleValue ? titleValue : title,
+            content: contentValue ? contentValue : content,
+            done: done,
+          };
+          console.log(body);
+          const response = await axios.patch(`/${_id}`, body);
+
+          const data = response.data;
+          const ListPage = await TodoList();
+          data && document.querySelector("#page").replaceWith(ListPage);
+        } catch (error) {
+          console.error("error");
+        }
+      });
+
+      return page;
+    }
+  } catch (error) {
+    console.error("error");
+  }
+};
+export default TodoUpdate;
