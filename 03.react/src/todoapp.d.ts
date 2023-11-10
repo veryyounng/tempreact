290133232534260a55055924e6eaf92a07d1a078
Pearl Kim <jinju000202@gmail.com>
2023-11-10 13:28:34 +0900

    chore: axios 설치

---
+++ 03.react/src/todoapp.d.ts
@@ -0,0 +1,26 @@
+interface TodoItem {
+  _id: number;
+  title: string;
+  content: string;
+  done: true;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// 리스트 조회 response
+interface TodoListResponse {
+  ok: number;
+  items: TodoItem[];
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+}
+
+// 상세조회, 수정, 등록 response
+interface TodoResponse {
+  ok: number;
+  item: TodoItem;
+}
