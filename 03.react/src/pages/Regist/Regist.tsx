890783b1dda90df5b067decbe7c668a32947fafe
kimzeze <fridaynight@kakao.com>
2023-11-10 17:02:21 +0900

    Design: 등록 페이지 레이아웃 일부 수정

--- 03.react/src/pages/Regist/Regist.tsx
+++ 03.react/src/pages/Regist/Regist.tsx
@@ -1,23 +1,15 @@
 import { Header } from "../../layout/Header/Header";
 import Button from "@/components/Button/Button";
 import styles from "./Regist.module.css";
+import { Input } from "@/components/Input/Input";
 
 export const Regist = () => {
   return (
     <div className={`${styles.registPage}`}>
-      <Header>투두 등록 헤더 테스트</Header>
+      <Header>TODO App 등록</Header>
       <div className={styles.registContainer}>
-        <input
-          type="text"
-          // value={title}
-          // onChange={(e) => setTitle(e.target.value)}
-          placeholder="제목"
-        />
-        <textarea
-          // value={content}
-          // onChange={(e) => setContent(e.target.value)}
-          placeholder="내용"
-        />
+        <Input label="제목" />
+        <Input label="내용" />
         <Button
           option="regist"
           label="등록"
