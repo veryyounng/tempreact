973d7cade0a663b764f6f23a5fe831ac89fded06
veryyounng <veryyounng>
2023-11-12 19:56:26 +0900

    <Feat: list 페이지 구현>-a

--- 03.react/src/routes.tsx
+++ 03.react/src/routes.tsx
@@ -7,6 +7,8 @@ import { RootLayout } from "./layout/RootLayout/RootLayout";
 import { Regist } from "./pages/Regist/Regist";
 import { List } from "./pages/List/List";
 import { Update } from "./pages/Update/Update";
+// import { TodoInfo } from "./pages/TodoInfo/TodoInfo";
+import NotFound from "./pages/error/NotFound";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,6 +16,8 @@ export const router = createBrowserRouter(
       <Route index element={<List />} />
       <Route path="regist" element={<Regist />} />
       <Route path="update/:todoId" element={<Update />} />
+      {/* <Route path="todoInfo/:todoId" element={<TodoInfo />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
